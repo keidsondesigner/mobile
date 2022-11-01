e66c20bfbea042a7bd4fafd5c758318bdbc3281f
keidsondesigner <keidsondesigner@gmail.com>
2022-10-31 23:01:24 -0400

    configuração do theme e fonts

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 
 import { NativeBaseProvider, Text, Center, VStack } from 'native-base';
 
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { THEME } from './src/styles/theme';
+
 export default function App() {
+	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-		<NativeBaseProvider>
+		<NativeBaseProvider theme={THEME}>
 			<Center 
 				flex={1}
-				bgColor="tertiary.900"
+				bgColor="gray.900"
 			>
 				<Text color="white" fontSize={20}>
 					Hello Word!
