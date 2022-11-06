03bc0755b0e38dbdb1a6f81ef0520273cf2970ab
keidsondesigner <keidsondesigner@gmail.com>
2022-11-06 19:02:22 -0400

    Tela de New

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -15,7 +15,7 @@ export function SignIn(){
 			<Button 
 				title="ENTRAR COM GOOGLE"
 				leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
-				type="DANGER"
+				type="SECONDARY"
 				mt={12}
 				onPress={signIn}
 			/>
